65276128d2dd19960cc7790c6033e79475b6f585
Simon Bächler <sb@feinheit.ch>
2013-02-07 12:58:45 +0100

    new content

--- newswall/contents.py
+++ newswall/contents.py
@@ -1,20 +1,35 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 from django.template import RequestContext
 from django.template.loader import render_to_string
-from django.utils.translation import ugettext_lazy as _
 
-from newswall.models import Story
-
+from .models import Story, Source
 
 class NewswallContent(models.Model):
+    num_stories = models.PositiveSmallIntegerField(_('stories'), default=3)
+
     class Meta:
         abstract =  True
         verbose_name = _('Newswall')
         verbose_name_plural = _('Newswalls')
 
     def render(self, request, **kwargs):
+        stories = []
+        sources = Source.objects.filter(show_min__gt=0)
+        # no sanity check here for performance reasons
+        for source in sources:
+            stories.extend(source.stories.active()[:source.show_min])
+        others = Story.objects.active()[:self.num_stories]
+        # fill up the list with remaining new stories.
+        for story in others:
+            if len(stories) < self.num_stories:
+                if story not in stories:
+                    stories.append(story)
+            else:
+                break
+
         context = {
-            'stories': Story.objects.active()[:3]
+            'stories': stories
         }
         return render_to_string([
             'content/newswall/{0}_default.html'.format(self.region),
